6d0e3c2a664b2723b33ac283809e54b0c61aef44
Ellezique <gizellevanzyl@gmail.com>
2021-06-29 21:49:24 +1000

    Add dinosaur image search API to clint side in script.js (fetch data using route created in index.js) and add getDinoImage to button click event listener

--- public/script.js
+++ public/script.js
@@ -1,13 +1,15 @@
 //CLIENT SIDE
 console.log('script.js loaded');
 
+//Event listener for button click
 document.querySelector('#btnLoad').addEventListener('click', () => {
   getDinoName();
+  getDinoImage;
 });
 
 getDinoName(); //execute getDinoName function 
 
-//DINO NAME
+//DINO NAME 
 //create get request to get data from server. fetch using route created in index.js
 async function getDinoName(){
   const response = await fetch('/dinoname');
@@ -16,4 +18,16 @@ async function getDinoName(){
   //returns array of two strings for dino name. Convert to string of words seperated by space:
   let dinoName = data[0].join(" ");
   console.log(dinoName);
+}
+
+
+//DINO IMAGE
+//create get request to get data from server. fetch using route created in index.js
+async function getDinoImage(){
+  const response = await fetch('/dinoimage');
+  //promise to JSON
+  const data = await response.json();
+  //returns array of two strings for dino name. Convert to string of words seperated by space:
+  let dinoImage = data[0];
+  console.log(dinoImage);
 }
