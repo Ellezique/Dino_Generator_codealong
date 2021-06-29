7127caf1033c5fdf8bdf22244d0533cb4a4f18d8
Ellezique <gizellevanzyl@gmail.com>
2021-06-29 21:33:01 +1000

    return dino name as two words seperated by space & add button with event listener.

--- public/script.js
+++ public/script.js
@@ -1,6 +1,10 @@
 //CLIENT SIDE
 console.log('script.js loaded');
 
+document.querySelector('#btnLoad').addEventListener('click', () => {
+  getDinoName();
+});
+
 getDinoName(); //execute getDinoName function 
 
 //create get request to get data from server. fetch using route created in index.js
@@ -8,5 +12,7 @@ async function getDinoName(){
   const response = await fetch('/dinoname');
   //promise to JSON
   const data = await response.json();
-  console.log(data);
+  //returns array of two strings for dino name. Convert to string of words seperated by space:
+  let dinoName = data[0].join(" ");
+  console.log(dinoName);
 }
