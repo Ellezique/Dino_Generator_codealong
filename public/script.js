39fe789edcc9efa914ecf161b4809ecb1828167e
Ellezique <gizellevanzyl@gmail.com>
2021-06-29 18:24:53 +1000

    install node fetch and add getDinoName fetchto client side script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
+//CLIENT SIDE
 console.log('script.js loaded');
+
+getDinoName(); //execute getDinoName function 
+
+//create get request to get data from server. fetch using route created in index.js
+async function getDinoName(){
+  const response = await fetch('/dinoname');
+  //promise to JSON
+  const data = await response.json();
+  console.log(data);
+}
